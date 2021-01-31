16421e028caf02f541d986ac1486b9c6c51c78bc
PROJWEB2 student <njar2008@student.iepscf-uccle.be>
2021-01-31 19:54:07 +0100

    Page Html ok

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = new express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Afrifoods' });
+});
+
+/* GET home page. */
+router.get('/Page', (req, res, next) => {
+  res.render('Afri-foods', { title: 'Afrifoods' });
 });
 
 module.exports = router;
